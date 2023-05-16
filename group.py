b7bbdd00333a3ce2e53fd038ea2948cedacb2007
Rohan Aditya <adityathirumala20@gmail.com>
2023-05-17 01:07:14 +0530

    Update group.py

--- group.py
+++ group.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Menu options
 menu_options = ["Home", "Attendance", "SGPA", "Contact"]
 st.title("Hi There!")
 # Display the menu
-s = st.selectbox("Select an Option!", menu_options)
+s = st.selectbox("Select an Option", menu_options)
 
 # Handle menu selections
 if s == "Home":
@@ -34,20 +36,45 @@ elif s == "Attendance":
     st.success(f"Attend {x} more Classes!!")
 
 elif s == "SGPA":
-    st.title("SGPA Estimator")
-    c = st.number_input("Enter your present CGPA", format="%f")
-    n = st.number_input("Enter number of Semesters held", min_value=1, format="%i")
-    e = st.number_input("Enter your target CGPA after this semester", format="%f")
-    s = 0
-    x = round(((n * c) + s) / (n + 1))
-    while x <= e:
-        s += 0.01
-        x = round(((n * c) + s) / (n + 1), 2)
-    s = round(s, 2)
-    if s > 10:
-        st.error("You cannot reach your expected CGPA!!")
-    else:
-        st.success(f"You need to score a minimum of {s} SGPA in your next semester")
+	st.title("SGPA Estimator")
+	c = st.number_input("Enter your present CGPA", format="%f")
+	n = st.number_input("Enter number of Semesters held", min_value=1, format="%i")
+	e = st.number_input("Enter your target CGPA after this semester", format="%f")
+	s = 0
+	x = round(((n * c) + s) / (n + 1))
+	while x <= e:
+		s += 0.01
+		x = round(((n * c) + s) / (n + 1), 2)
+	s = round(s, 2)
+	if s > 10:
+		st.error("You cannot reach your expected CGPA!!")
+	else:
+		st.success(f"You need to score a minimum of {s} SGPA in your next semester")
+	st.title("GPA graphs")
+	n=st.number_input("Enter number of semesters held",min_value=1, format="%i")
+	l1=[]
+	for i in range(n):
+		input_value = st.number_input(f'Enter Semester {i+1} SGPA', key=f'input_{i+1}')
+		l1.append(input_value)
+
+	l2=[]
+	avg=0
+	for i in range(n):
+		for j in range(i+1):
+			avg+=l1[j]
+		avg=avg/(j+1)
+		l2.append(avg)
+		avg=0
+		
+	semesters = [i+1 for i in range(len(l1))]
+	plt.plot(semesters, l1,label='SGPA',marker='o',color='r')
+	plt.plot(semesters, l2,label='CGPA',marker='o',color='k')
+	plt.xlabel('Semester')
+	plt.ylabel('GPA')
+	plt.xticks(np.arange(1,n+1,1))
+	plt.title('Semester SGPAs and CGPAs')
+	plt.legend()
+	st.pyplot(plt)
 
 elif s == "Contact":
 	st.markdown("""<a href='https://github.com/aytid'>Github</a>""", unsafe_allow_html=True)
