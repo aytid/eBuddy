379024c3f1c462fbe39b957fa90df368030003af
Rohan Aditya <adityathirumala20@gmail.com>
2024-12-08 13:11:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,17 +16,8 @@ selected_option = st.selectbox(
 if selected_option == "Home":
     st.title("eBuddy")
     st.subheader("Simplify Your Academic Life!")
-    st.markdown("Know your attendance status before and after bunking")
-    st.markdown("Calculate the SGPA needed for your desired CGPA")
-
-    '''col1, col2 = st.columns(2)
-    with col1:
-        #st.image("attendance.png", caption="Track Attendance", use_column_width=False, width=250)
-        st.markdown("### Know your attendance status before and after bunking!")
-    with col2:
-        #st.image("score.png", caption="Estimate SGPA", use_column_width=False, width=250)
-        st.markdown("### Calculate the SGPA needed for your desired CGPA!")
-        '''
+    st.subheader("Know your attendance status before and after bunking")
+    st.subheader("Calculate the SGPA needed for your desired CGPA")
         
 
 # Attendance Page
